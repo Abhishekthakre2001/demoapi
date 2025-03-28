90dd77c6c43c06d7d05a92c20d695a46b89361d6
Abhishek Thakre <abhishekpthakre2023@gmail.com>
2025-03-28 23:13:48 +0530

    ok
    ok

--- Backend/routes/SuperAdmin.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { superadminlogin } = require('../controllers/SuperAdminController')
-
-
-router.post("/login", superadminlogin);
-
-module.exports = router;
